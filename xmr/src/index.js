d6c6a9ababf252c984d6ed6f78f1ad0287f071b9
Pau Aliagas <linuxnow@gmail.com>
2026-02-10 01:38:59 +0100

    fix: move XmrWrapper to xmr package and fix export
    - Move xmr-wrapper.js to packages/xmr/src/
    - Export XmrWrapper as named export (not default)
    - Fixes 'XmrWrapper is not a constructor' error

--- xmr/src/index.js
+++ xmr/src/index.js
@@ -1,12 +1,2 @@
 // @xiboplayer/xmr - XMR WebSocket client
-// XMR implementation will be added here
-export default class XMRClient {
-  constructor(options) {
-    this.options = options;
-  }
-
-  async connect() {
-    // Implementation coming soon
-    console.log('XMR client - implementation pending');
-  }
-}
+export { XmrWrapper } from './xmr-wrapper.js';
